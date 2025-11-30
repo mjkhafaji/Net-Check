cf995f9566adf9fea448d1a8b30298a219f120dc
Mohammed Al-Khafaji <malkh14@wgu.edu>
2025-11-30 14:24:11 -0600

    feat: add CLI arguments for dynamic IP scanning

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 def check_server(address, port):
     # Create a TCP socket
     s = socket.socket()
+    s.settimeout(2) # Give up after 2 seconds
     print(f"Attempting to connect to {address} on port {port}...")
     
     try:
@@ -12,11 +13,18 @@ def check_server(address, port):
         return True
     except socket.error as e:
         print(f"FAILED: Connection to {address} on port {port} failed")
-        print(f"Error: {e}")
         return False
     finally:
         s.close()
 
 if __name__ == '__main__':
-    # Default check google.com on port 80 (HTTP)
-    check_server("google.com", 80)
+    # Check if the user provided enough arguments
+    # sys.argv[0] is the script name, [1] is the IP, [2] is the Port
+    if len(sys.argv) != 3:
+        print("Usage: python main.py <TARGET_IP> <PORT>")
+        sys.exit(1)
+
+    target_ip = sys.argv[1]
+    target_port = int(sys.argv[2]) # Convert text to number
+
+    check_server(target_ip, target_port)
